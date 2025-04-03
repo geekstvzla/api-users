07abc7d3eaa3bd2f6f3cf0d43fdc5dcb04765a21
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2025-04-03 11:17:53 -0400

    update

--- database/inserts/status.sql
+++ database/inserts/status.sql
@@ -1,3 +1,3 @@
 INSERT INTO `status` (`value`, `table`, `description`, `available`) VALUES ('1', 'users', 'active', '1');
 INSERT INTO `status` (`value`, `table`, `description`, `available`) VALUES ('2', 'users', 'inactive', '1');
-INSERT INTO `status` (`value`, `table`, `description`, `available`) VALUES ('0', 'users', 'needs verification', '1');
+INSERT INTO `status` (`value`, `table`, `description`, `available`) VALUES ('3', 'users', 'needs verification', '1');
--- database/stores_procedures/sp_sign_in.sql
+++ database/stores_procedures/sp_sign_in.sql
@@ -8,14 +8,7 @@ BEGIN
     
     IF @v_user_exists > 0 THEN
 
-        SELECT `iv`,
-               `key`
-        INTO @v_encrypt_iv,
-             @v_encrypt_key
-        FROM vw_encryption_variables;
-
         SELECT u.user_id,
-        	   CAST(AES_DECRYPT(u.password, @v_encrypt_key) AS CHAR),
                u.username,
                IF(
                    u.avatar IS NULL OR u.avatar = '',
@@ -25,7 +18,6 @@ BEGIN
                u.status_id,
                s.description
         INTO @v_user_id,
-             @v_password,
              @v_username,
              @v_avatar,
              @v_user_status_id,
@@ -34,72 +26,55 @@ BEGIN
         INNER JOIN status s ON s.value = u.status_id
         AND s.table = "users"
         WHERE u.email = p_email;
-        
-        IF p_password = @v_password THEN
 
-            IF @v_user_status_id = 3 THEN
-				
-                SELECT fn_messages("SP_SING_IN", 3, 1, p_lang_id) INTO @v_message_data;
-				SELECT JSON_UNQUOTE(JSON_EXTRACT(@v_message_data, '$.message')) INTO @v_message;
-        
-                SELECT CONCAT('{
-                    "response" : {
-					    "message"    : "',@v_message,'",
-				        "status"     : "warning",
-				        "statusCode" : 3,
-                        "userId"     : ',@v_user_id,'
-                    }
-			    }') INTO p_response;
-
-            ELSEIF @v_user_status_id = 1 THEN
-				
-                SELECT fn_messages("SP_SING_IN", 1, 1, p_lang_id) INTO @v_message_data;
-				SELECT JSON_UNQUOTE(JSON_EXTRACT(@v_message_data, '$.message')) INTO @v_message;
-                
-            	SELECT CONCAT('{
-                    "response" : {
-					    "avatar"     : "',@v_avatar,'",
-                        "message"    : "',@v_message,'",
-                        "username"   : "',@v_username,'",
-				        "status"     : "success",
-				        "statusCode" : 1,
-                        "userId"     : ',@v_user_id,'
-					}
-			    }') INTO p_response;
-
-            ELSE
-				
-                SELECT fn_messages("SP_SING_IN", 2, 1, p_lang_id) INTO @v_message_data;
-				SELECT JSON_UNQUOTE(JSON_EXTRACT(@v_message_data, '$.message')) INTO @v_message;
-                
-            	SELECT CONCAT('{
-                    "response" : {
-					    "message"    : "',@v_message,' <b>',@v_user_status_desc,'</b>",
-				        "status"     : "warning",
-				        "statusCode" : 2
-					}
-			    }') INTO p_response;
-
-            END IF;
-
-        ELSE
+		IF @v_user_status_id = 3 THEN
 			
-            SELECT fn_messages("SP_SING_IN", 4, 1, p_lang_id) INTO @v_message_data;
+			SELECT fn_messages("SP_SING_IN", 3, 1) INTO @v_message_data;
 			SELECT JSON_UNQUOTE(JSON_EXTRACT(@v_message_data, '$.message')) INTO @v_message;
-                
-            SELECT CONCAT('{
-                "response" : {
-		            "message"    : "',@v_message,'",
-		            "status"     : "error",
-                    "statusCode" : 4
-                }
-		    }') INTO p_response;
+		
+			SELECT CONCAT('{
+				"response" : {
+					"message"    : "',@v_message,'",
+					"status"     : "warning",
+					"statusCode" : 3,
+					"userId"     : ',@v_user_id,'
+				}
+			}') INTO p_response;
 
-        END IF;
+		ELSEIF @v_user_status_id = 1 THEN
+			
+			SELECT fn_messages("SP_SING_IN", 1, 1) INTO @v_message_data;
+			SELECT JSON_UNQUOTE(JSON_EXTRACT(@v_message_data, '$.message')) INTO @v_message;
+			
+			SELECT CONCAT('{
+				"response" : {
+					"avatar"     : "',@v_avatar,'",
+					"message"    : "',@v_message,'",
+					"username"   : "',@v_username,'",
+					"status"     : "success",
+					"statusCode" : 1,
+					"userId"     : ',@v_user_id,'
+				}
+			}') INTO p_response;
+
+		ELSE
+			
+			SELECT fn_messages("SP_SING_IN", 2, 1) INTO @v_message_data;
+			SELECT JSON_UNQUOTE(JSON_EXTRACT(@v_message_data, '$.message')) INTO @v_message;
+			
+			SELECT CONCAT('{
+				"response" : {
+					"message"    : "',@v_message,' <b>',@v_user_status_desc,'</b>",
+					"status"     : "warning",
+					"statusCode" : 2
+				}
+			}') INTO p_response;
+
+		END IF;
 
     ELSE
 		
-        SELECT fn_messages("SP_SING_IN", 0, 1, p_lang_id) INTO @v_message_data;
+        SELECT fn_messages("SP_SING_IN", 0, 1) INTO @v_message_data;
 		SELECT JSON_UNQUOTE(JSON_EXTRACT(@v_message_data, '$.message')) INTO @v_message;
         
         SELECT CONCAT('{
