bd25799bc9bd45b5263679a7b0594529961fd9b8
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2026-01-18 23:36:23 -0400

    updateee

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/users', usersRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('Geek ST API');
+    res.send('Geek ST API '+${process.env.API_PORT});
 });
 
 app.listen(process.env.API_PORT, () => {
